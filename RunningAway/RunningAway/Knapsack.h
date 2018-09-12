2f9f16439a58a6b410845e8cac62dfb9c6354b20
LuisGH1234 <u201516808@upc.edu.pe>
2018-09-11 22:58:51 -0500

    se implemento el algoritmo

--- RunningAway/RunningAway/Knapsack.h
+++ RunningAway/RunningAway/Knapsack.h
@@ -4,18 +4,22 @@
 #include <vector>
 
 using namespace std;
+using namespace System;
 
 struct Item
 {
-	int costo;
-	int peso;
+	int costo = 0;
+	int peso = 0;
+	bool selected = false;
+	Item(int costo, int peso) : costo(costo), peso(peso) {}
 };
 
 class Knapsack {
 private:
 	int pesoMax;
+	bool found;
 	vector<Item> items;
-	vector<Item> mochila;
+	vector<Item> _Mochila;
 
 public:
 	Knapsack()
@@ -35,15 +39,84 @@ public:
 
 	int GetPesoMax() { return this->pesoMax; }
 	vector<Item> GetItems() { return this->items; }
-	vector<Item> GetMochila() { return this->mochila; }
+	vector<Item> GetMochila() { return this->_Mochila; }
 	void SetPesoMax(int pesoMax) { this->pesoMax = pesoMax; }
 	void SetItems(vector<Item> items) { this->items = items; }
 
-	void HillClimbing(int pesoActual = 0, int pos = 0)
+	bool allSelected(vector<Item> items) {
+		for (int i = 0; i < items.size(); i++)
+		{
+			if (items[i].selected == false)
+				return false;
+		}
+		return true;
+	}
+
+	vector<Item> resetItems(vector<Item> items) {
+		for (int i = 0; i < items.size(); i++)
+		{
+			items[i].selected = false;
+		}
+		return items;
+	}
+private:
+	vector<Item> initItems() 
 	{
-		if (pesoActual == pesoMax) return;
+		vector<Item> aux;
+		aux.push_back(Item(8, 5));
+		aux.push_back(Item(7, 6));
+		aux.push_back(Item(12, 10));
+		aux.push_back(Item(6, 4));
+		aux.push_back(Item(2, 1));
+		aux.push_back(Item(3, 1));
+		return aux;
+	}
 
+	void HillClimbing(int pesoActual, int pos, vector<Item> mochila, vector<Item> items)
+	{
+		if (found == true) return;
+		if (pesoActual == pesoMax) {
+			found = true;
+			_Mochila = mochila;
+		}
+		if (pos >= items.size() || allSelected(items) == true || items[pos].selected == true)
+			return;
 
+		mochila.push_back(items[pos]);
+		items[pos].selected = true;
+		pesoActual += items[pos].peso;
+
+		if (pesoActual > pesoMax) return;
+
+		for (int i = 0; i < items.size(); i++)
+		{
+			HillClimbing(pesoActual, i, mochila, items);
+			if (found == true) return;
+		}
+		HillClimbing(0, pos + 1, {}, resetItems(items));
+	}
+
+	int sumaPesoTotalItems() 
+	{
+		int aux = 0;
+		for (int i = 0; i < items.size(); i++)
+		{
+			aux += items[i].peso;
+		}
+		return aux;
+	}
+public:
+	vector<Item> HillClimbing() 
+	{
+		if (pesoMax > sumaPesoTotalItems())
+			return items;
+		HillClimbing(0, 0, {}, initItems());
+		String ^cad = gcnew String("");
+		for (int i = 0; i < _Mochila.size(); i++)
+		{
+			cad += "costo: " + _Mochila[i].costo + " - peso: " + _Mochila[i].peso + "\n";
+		}
+		return _Mochila;
 	}
 };
 
